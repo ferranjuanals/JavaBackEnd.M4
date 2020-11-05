7deeffa3bca859c632a22fb347400d25612999c6
Ferran Juanals <ferranjuanals@gmail.com>
2020-11-05 15:51:37 +0100

    Exercici JavaBackEnd.M4 - Fase 1

--- src/RestaurantApp.java
+++ src/RestaurantApp.java
@@ -2,7 +2,14 @@
 public class RestaurantApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Crear variables pels bitllets d'€ que hi ha i pel preu total
+		int bitllet1=5, bitllet2=10, bitllet3=20, bitllet4=50, bitllet5=100, bitllet6=200, bitllet7=500;
+		double preuTotal;
+		
+		// Crear dos Arrays pel menú i preu de cada plat
+		String[] menu;
+		double[] preuPlat;
 
 	}
 
