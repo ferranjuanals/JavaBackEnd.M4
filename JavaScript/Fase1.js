72e22064f1839e390bb9b60df61bf963910851b1
Ferran Juanals <ferranjuanals@gmail.com>
2020-11-10 16:52:48 +0100

    Exercici JavaBackEnd.M4 - Fase 1 (JavaScript)

---
+++ JavaScript/Fase1.js
@@ -0,0 +1,8 @@
+
+// Crear variables pels bitllets d'€ que hi ha i pel preu total
+var bitllet1=5, bitllet2=10, bitllet3=20, bitllet4=50, bitllet5=100, bitllet6=200, bitllet7=500;
+var preuTotal = 0;
+
+// Crear dos Arrays pel menú i preu de cada plat
+var menu = new Array(6);
+var preuPlat = new Array(6);
